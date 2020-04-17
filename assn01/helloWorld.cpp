8eee570e482c81618fbede47f7f856af8ad440dd
adoxic <griffinmaeve@gmail.com>
2020-04-17 14:42:40 -0700

    finished hello worlds

--- assn01/helloWorld.cpp
+++ assn01/helloWorld.cpp
@@ -6,18 +6,26 @@ int main()
 {
   //print "Hello World"
   string hi = "Hello World";
-  cout << hi;
+  cout << hi << " just one \n";
 
   //print "Hello World" using for, while, and do...while loops
   int n = 3;
   for(int i = 0; i < n; i++) 
   {
-    cout << hi << "for \n";
+    cout << hi << " for \n";
   }
   
   int i = 1;
   while(i < n)
   {
-    cout << hi << "while \n";
+    cout << hi << " while \n";
+    i++;
   }
+
+  do
+  {
+    cout << hi << " do while \n";
+    i--;
+  } while(i > 1);
+  
 }

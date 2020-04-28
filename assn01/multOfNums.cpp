99ba095772ae92237cc714cff24de5efd4aeea89
adoxic <griffinmaeve@gmail.com>
2020-04-28 16:20:33 -0700

    3.4.3

--- assn01/multOfNums.cpp
+++ assn01/multOfNums.cpp
@@ -4,19 +4,28 @@ using namespace std;
 
 int main()
 {
-  while(true)
+  bool keepGoing;
+  while(keepGoing)
   {
     int numToEvaluate;
     cout << "Input number to see if it is devisible by 5:";
     cin >> numToEvaluate;
+    
+    if(numToEvaluate < 0) 
+    {
+      keepGoing = false;
+      continue;
+    }
 
     if(numToEvaluate % 5 == 0) 
     {
       cout << "5 \n"; 
-    } else 
+    } 
+    else 
     {
       continue;
     }
  
   }
+  cout << "Goodbye!";
 }

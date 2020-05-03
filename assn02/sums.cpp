7261807cba62ceb0131319a510428773f205fe45
adoxic <griffinmaeve@gmail.com>
2020-05-02 17:20:17 -0700

    4.5

--- assn02/sums.cpp
+++ assn02/sums.cpp
@@ -10,6 +10,14 @@ int sum(int a, int b, int c = 0, int d = 0) {
   return a + b + c + d;
 }
 
+int sum(const int numbers[], const int numbersLen) {
+  int total;
+  for(int i = 0; i < numbersLen; i++) {
+    total += numbers[i];
+  }
+  return total;
+}
+
 int main() {
   cout << sum(3, 2) << "\n";
 
@@ -18,4 +26,7 @@ int main() {
   cout << sum(2, 4) << "\n";
 
   cout << sum(2, 4, 3, 2) << "\n";
+  
+  int foo [3] = {16, 2, 10};
+  cout << sum(foo, 3) << "\n";
 }

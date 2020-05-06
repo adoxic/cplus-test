29b90abc21c13dee1b8e554e9bedd90fcbe4b9be
adoxic <griffinmaeve@gmail.com>
2020-05-06 14:37:40 -0700

    not sure if I did this function right

--- assn02/pi.cpp
+++ assn02/pi.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int calcHits(const int coordinates[]) {
   
 }
-
+int n = 5;
 int main() {
-  for (int i = 0; i < 5; i++)
-  {
-    int coordinates[2] = {rand() % 100, rand() % 100};
+  int hits = 0;
+  for (int i = 0; i < n; i++) {
+    double x = rand() /  (double)RAND_MAX, y = rand() / (double)RAND_MAX;
+    cout << x << " " << y << '\n';
+
+    if(sqrt(x * x + y * y) < 1) {
+      ++hits;
+    }
   }
-  
+  cout << "hits " << hits << "\n";
 }
